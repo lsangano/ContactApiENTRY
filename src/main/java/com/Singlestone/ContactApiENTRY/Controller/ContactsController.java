4533b5b9a342fb1159f20a16fc87ef04c2496a17
lsangano <ndutilae@gmail.com>
2021-05-19 17:07:28 -0400

    hello

--- src/main/java/com/Singlestone/ContactApiENTRY/Controller/ContactsController.java
+++ src/main/java/com/Singlestone/ContactApiENTRY/Controller/ContactsController.java
@@ -32,18 +32,6 @@ public class ContactsController {
         return resp;
     }
 
-    @PutMapping("contacts/{id}")
-    public Contact updateContact(@PathVariable Long id, @RequestBody Contact contact) {
-        if (id == null)
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Id has been provided.");
-        Optional<Contact> searchForContact = contactDao.findById(id);
-        if(! searchForContact.isPresent())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Provided Contact ID is invalid/does not exist - cannot update.");
-        contact.setId(id);
-        Contact resp = contactDao.save(contact);
-        return resp;
-    }
-
     @GetMapping("contacts/{id}")
     public Contact getContact(@PathVariable Long id) {
         Optional<Contact> searchForContact = contactDao.findById(id);
